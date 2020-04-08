08ccb9a7413f2d913fed733010a4924303f1b0db
Andrés Felipe Chaparro <andres.chaparro06@uptc.edu.co>
2020-04-07 20:06:15 -0500

    Primer esquema

--- index.php
+++ index.php
@@ -2,9 +2,41 @@
 <html>
 <head>
 	<title>Awesome Tourney</title>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<style type="text/css">
+		body {
+		    display: flex;
+		    flex-wrap: wrap;
+		    min-height: 100vh
+		}
+
+		footer {
+		    align-self: flex-end;
+		}
+	</style>
 </head>
 
 <body>
+	<header class="header col-lg-12">
+		<h1>HEADER</h1>
+	</header>
 
+	<content style="float: left; margin-bottom: 20px;" class="col-lg-12">
+		<div class="row">
+			<div class="col-lg-12">
+				<h2>CONTENT</h2>
+			</div>
+		</div>
+	</content>
+
+	<footer class="footer col-lg-12">
+		<h4>FOOTER</h4>
+	</footer>
+
+	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </body>
 </html>
