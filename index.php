3f43df1bb32e792eaaac8e0976523f6eccad972c
Andrés Felipe Chaparro Rosas <36827424+AFelipeCR@users.noreply.github.com>
2020-04-10 15:43:08 -0500

    Update index.php

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
 	</content>
 
 	<footer class="footer col-lg-12">
-		<h4>FOOTER - Yohan estuvo aquí</h4>
+		<h4>FOOTER</h4>
 	</footer>
 
 	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
